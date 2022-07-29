f62c477293edaf059843344455a24fa6728a8f0a
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-07-29 17:46:17 +0200

    creation of the first model + migrate

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class ModelPerson(models.Model):
+    class LandChoices(models.TextChoices):
+        Belgique = "BE",
+        France = "Fr",
+        Allemagne = "DE",
+        Italie = "IT"
+
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(100)])
+    email = models.EmailField()
+    english = models.BooleanField(default=False)
+    land = models.CharField(choices=LandChoices.choices, max_length=25)
