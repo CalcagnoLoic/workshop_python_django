1bceeaf47afba6d2e33d2fb65e37b2bf044b2b38
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-07-29 18:01:11 +0200

    creation of admin and superuser

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from app1.models import ModelFilm
+
+
+class AdminFilm(admin.ModelAdmin):
+    list_display = ("id", "name", "year", "author", "english", "land_production", "official_page")
+
+
+admin.site.register(ModelFilm, AdminFilm)
