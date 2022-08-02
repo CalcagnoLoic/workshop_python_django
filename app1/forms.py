d6535ebd9d82c517735a14cec84163c2b0fede61
Anaïs.B <95853397+Nymphadorart@users.noreply.github.com>
2022-08-02 16:15:48 +0200

    Update forms.py

--- app1/forms.py
+++ app1/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from app1.models import ModelFilm
 
 
-class AddFilm(forms.Form):
+class AddFilm(forms.ModelForm):
     class Meta:
         model = ModelFilm
         fields = '__all__'
